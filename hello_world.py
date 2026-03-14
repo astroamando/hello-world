39cf4ff079ff073c16ef2c372ca7ca0c9b9727f7
astroamando <amando.filho83@gmail.com>
2026-03-14 16:29:41 -0600

    program and read me cleaned up (awaiting video link)

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,7 @@
-print("Hello World!")
+# Hello World program for CSE 310
+# Author: Amando Evangelista Santos Filho
+# This program prints a greeting message to the screen.
+
+print("\nHello World!")
 print("My name is Amando Evangelista Santos Filho.")
-print("I am a Software Development student at BYU-Pathway.")
+print("I am a Software Development student at BYU-Pathway.\n")
